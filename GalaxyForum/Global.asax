a07d9196c465e5da90174cbecadeb0fe894e3215
User <User@DESKTOP-P32JFV3>
2017-06-18 20:39:17 +0300

    folder "Galaxy Forum" was removed. it's needless project

--- GalaxyForum/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GalaxyForum.MvcApplication" Language="C#" %>
--- GalaxyForum/Models/MvcSection.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GalaxyForum.Models
-{
-    public class MvcSection
-    {
-        public int Id { get; set; }
-
-        public string Name { get; set; }
-    }
-}
